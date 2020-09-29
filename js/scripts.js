633ac6efd28f1e9e568346e8b31d4b794a191a9e
diegotorres9 <diegoetorres1@gmail.com>
2020-09-29 19:56:35 -0400

    updating zoom level on map

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-var mymap = L.map('location-map').setView([40.105, -75.345], 13);
+var mymap = L.map('location-map').setView([40.105, -75.345], 12);
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     maxZoom: 18,
     id: 'mapbox/streets-v11',
