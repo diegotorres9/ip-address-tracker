f9cfcf632db36c37b8a2dd991fcbd07c0fc96944
diegotorres9 <diegoetorres1@gmail.com>
2020-10-01 21:30:32 -0400

    new fetch function and new function to put data inside of html

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,39 @@
 // fetch request from geo ipify
-fetch('https://geo.ipify.org/api/v1?apiKey=at_3qU3a2WWwxwh36bo19rAfTgCWwnRA&ipAddress=')
-.then(response => response.json())
-.then(data => console.log(data));
+// fetch('https://geo.ipify.org/api/v1?apiKey=at_3qU3a2WWwxwh36bo19rAfTgCWwnRA&ipAddress=')
+// .then(response => response.json())
+// .then(data => obj = data)
+// .then(() => {
+//     // Initialize our ip object variables
+//     var userIP = obj.ip;
+//     var userLocation = obj.location.region + ', ' + obj.location.region + ' ' + obj.location.postalCode;
+//     var userTimezone = 'UTC ' + obj.location.timezone;
+//     var userISP = obj.isp;
+
+//     // create variables from html elements
+//     var ipAddressHtml = document.querySelector('.ip-address');
+//     var locationHtml = document.querySelector('.location-span');
+//     var timezoneHtml = document.querySelector('.timezone-span');
+//     var ispHtml = document.querySelector('.isp-span');
+
+//     // update the html with the corresponding object data
+//     ipAddressHtml.innerHTML = userIP;
+//     locationHtml.innerHTML = userLocation;
+//     timezoneHtml.innerHTML = userTimezone;
+//     ispHtml.innerHTML = userISP;
+// });
+function getIpAPI(url, callback) {
+    var ipObj;
+    fetch(url)
+    .then(response => response.json())
+    .then(data => ipObj = data)
+    .then(() => callback(ipObj));
+}
+
+getIpAPI('https://geo.ipify.org/api/v1?apiKey=at_3qU3a2WWwxwh36bo19rAfTgCWwnRA&ipAddress=', getIpData);
+function getIpData(data) {
+    document.querySelector('.ip-address').innerHTML = data.ip;
+    document.querySelector('.location-span').innerHTML = data.location.city + ', ' + data.location.region + ' ' + data.location.postalCode;
+    document.querySelector('.timezone-span').innerHTML = 'UTC ' + data.location.timezone;
+    document.querySelector('.isp-span').innerHTML = data.isp;
+}
+
