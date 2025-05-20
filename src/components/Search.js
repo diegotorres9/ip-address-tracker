6cd927c7fc55b1ef6fce977c27325fa21a171544
Diego Torres <diegoetorres1@gmail.com>
2025-05-20 17:29:59 -0400

    cleaned up Search.js page

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,32 +1,19 @@
 'use client';
 import Image from "next/image";
 import styles from './search.module.css';
-import { geolocate } from "@/services/geolocation";
-import { useState } from "react";
-
-const locationData = await geolocate();
-// console.log(locationData);
-const ipAddress = locationData.ip;
-const isp = locationData.isp;
-const location = locationData.location.city;
-const timeZone = locationData.location.timezone;
+// import { geolocate } from "@/services/geolocation";
+// import { useEffect } from "react";
 
 
-export default function Search() {
-    const [inputValue, setInputValue] = useState('');
-
-    const handleSearch = (e) => {
-        setInputValue(e.target.value)
-    }
-    console.log(inputValue);
+export default function Search({geolocate, handleSearch}) {
     return (
         <section>
             <div className={styles.searchbar__container}>
+            <form onSubmit={geolocate}>
                 <input 
                     type="search" 
                     className={styles.address__searchbar}
                     placeholder="Search for any IP address or domain"
-                    value={inputValue}
                     onChange={handleSearch}>
                 </input>
                 <button
@@ -40,24 +27,7 @@ export default function Search() {
                     alt=""
                 />
                 </button>
-            </div>
-            <div>
-                <div>
-                    <span>IP ADDRESS</span>
-                    <p>{ipAddress}</p>
-                </div>
-                <div>
-                    <span>LOCATION</span>
-                    <p>{location}</p>
-                </div>
-                <div>
-                    <span>TIMEZONE</span>
-                    <p>{timeZone}</p>
-                </div>
-                <div>
-                    <span>ISP</span>
-                    <p>{isp}</p>
-                </div>
+            </form>
             </div>
         </section>
     )
