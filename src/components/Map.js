181c69967f753f6264908286f4ccd19dcf4bd15e
Diego Torres <diegoetorres1@gmail.com>
2025-05-20 17:29:30 -0400

    initialize map with geolocation call and then change latitude and longitude after user input change

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,26 +1,44 @@
 'use client';
 import { MapContainer, Marker, Popup, TileLayer, Tooltip } from "react-leaflet";
+import ChangeView from "./ChangeView";
 import "leaflet/dist/leaflet.css";
 import "leaflet-defaulticon-compatibility";
 import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css";
 import { geolocate } from "@/services/geolocation";
+import { useEffect } from "react";
 
+
+
+// const geoLat = coordinates.location.lat;
+// const geoLng = coordinates.location.lng;
 const coordinates = await geolocate();
-// console.log(coordinates.location);
-const latCoord = coordinates.location.lat;
-const lngCoord = coordinates.location.lng;
-// console.log(latCoord, lngCoord);
 
 
-export default function Map () {
-    
+export default function Map ({locationData}) {
+    useEffect(() => {}, [locationData]);
+
+
+    let latCoord = coordinates.location.lat;
+    let lngCoord = coordinates.location.lng;
+
+    // let geoLat;
+    // let geoLng;
+
+    if(locationData.length !== 0) {
+        latCoord = locationData.location.lat;
+        lngCoord = locationData.location.lng;
+    }
+
+    // console.log(geoLat, geoLng);
+
     return (
-        <MapContainer center={[latCoord,lngCoord]} zoom={12} scrollWheelZoom={false} style={{height: '800px'}}>
+        <MapContainer center={[latCoord, lngCoord]} zoom={12} scrollWheelZoom={false} style={{height: '800px'}}>
+            <ChangeView center={[latCoord, lngCoord]} zoom={12}/>
             <TileLayer
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
-            <Marker position={[latCoord,lngCoord]}>
+            <Marker position={[latCoord, lngCoord]}>
                 <Popup>
                     a customizable popup
                 </Popup>
