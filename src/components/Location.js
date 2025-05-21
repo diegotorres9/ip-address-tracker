f970f7e1cb8f42ffdd88d4ac36975b89440629a0
Diego Torres <diegoetorres1@gmail.com>
2025-05-21 10:19:24 -0400

    set var to initlal location fetch then update vars after user input fetch call

--- src/components/Location.js
+++ src/components/Location.js
@@ -1,14 +1,43 @@
 import { useEffect } from "react";
+import { geolocate } from "@/services/geolocation";
+import styles from './location.module.css';
+
+const locationInfo = await geolocate();
+// console.log(locationInfo);
 
 export default function Location({locationData}) {
-    useEffect(() => {}, [locationData])
+    useEffect(() => {}, [locationData]);
+
+    let locationIp = locationInfo.ip;
+    let locationIsp = locationInfo.isp;
+    let locationCity = locationInfo.location.city;
+    let locationTimeZone = locationInfo.location.timezone;
+
+    if(locationData.length !== 0) {
+        locationIp = locationData.ip;
+        locationIsp = locationData.isp;
+        locationCity = locationData.location.city;
+        locationTimeZone = locationData.location.timezone;
+    }
+
     return (
-        <div>
-            {/* <pre>{JSON.stringify(locationData, null, 2)}</pre> */}
-            <span>{locationData.ip}</span>
-            <span>{locationData.location?.city}</span>
-            <span>{locationData.location?.timezone}</span>
-            <span>{locationData.isp}</span>
+        <div className={styles.location__wrapper}>
+            <div className={styles.location__container}>
+                <span className={styles.location__heading}>IP ADDRESS</span>
+                <span className={styles.location__content}>{locationIp}</span>
+            </div>
+            <div className={styles.location__container}>
+                <span className={styles.location__heading}>LOCATION</span>
+                <span className={styles.location__content}>{locationCity}</span>
+            </div>
+            <div className={styles.location__container}>
+                <span className={styles.location__heading}>TIMEZONE</span>
+                <span className={styles.location__content}>{locationTimeZone}</span>
+            </div>
+            <div className={styles.location__container}>
+                <span className={styles.location__heading}>ISP</span>
+                <span className={styles.location__content}>{locationIsp}</span>
+            </div>
         </div>
     )
 }
