4ee79e11c18d8efec9a8598962ff3eba9c271892
Diego Torres <diegoetorres1@gmail.com>
2025-05-21 10:20:28 -0400

    add domain call to end of fetch url

--- src/services/geolocation.js
+++ src/services/geolocation.js
@@ -1,5 +1,5 @@
 export async function geolocate() {
-    const data = await fetch('https://geo.ipify.org/api/v1?apiKey=at_3qU3a2WWwxwh36bo19rAfTgCWwnRA&ipAddress=');
+    const data = await fetch('https://geo.ipify.org/api/v1?apiKey=at_3qU3a2WWwxwh36bo19rAfTgCWwnRA&ipAddress=&domain=');
     const coordData = await data.json();
     return coordData;
 }
